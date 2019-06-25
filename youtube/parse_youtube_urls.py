84df259b5c821f29ba38842271828c67871ed21b
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2019-06-25 11:09:02 +0200

    build links webentity -> YT_channel

--- youtube/parse_youtube_urls.py
+++ youtube/parse_youtube_urls.py
@@ -1,11 +1,26 @@
 
 import re
+import sys
+import json
+import time
+import requests
+import warnings
+warnings.filterwarnings("ignore")
 try:
     from urlib.parse import urlparse
+    from urllib import unquote
 except:
     from urlparse import urlparse
-from urllib2 import unquote
+    from urllib2 import unquote
+
 from ural import normalize_url
+
+CACHE_USERS_CHANNELS = {}
+try:
+    with open(".cache_users_channels.json") as f:
+        CACHE_USERS_CHANNELS = json.load(f)
+except:
+    pass
 
 def clean_url(url):
     if url.endswith("/live"):
@@ -107,19 +122,7 @@ def _extract_YT_video_id(u):
     l["yt_video_id"]  = [q.split("=")[1] for q in u.split("?")[-1].split("&") if q.startswith("v=")][0]
     pass
 
-def _extract_YT_channel_id(u):
-    u = u[12:]
-    if u.startswith("channel/"):
-        pass
-    elif u.startswith("user/"):
-        pass
-    elif u.startswith("c/"):
-        pass
-    elif "?" not in u or "v=" not in u:
-        pass
-    pass
-
-def get_channel_from_url(u):
+def get_channel_from_url(u, cache_videos_channels={}):
     try:
         typ, uid = parse_youtube_url(u)
         if typ in ("irrelevant", "error"):
@@ -127,31 +130,70 @@ def get_channel_from_url(u):
         if typ == "channel":
             return uid
         if typ == "user":
-            return get_channel_from_user_id(uid)
+            return get_channel_from_user_id(uid, u)
         if typ == "video":
-            return get_channel_from_video_id(uid)
+            return get_channel_from_video_id(uid, cache_videos_channels)
         raise(Exception("ERROR: %s does not seem attachable to a YouTube channel" % u))
     except Exception as e:
         pass
 
-def get_channel_from_user_id(uid):
-    pass
+re_link_canonical = re.compile(r'<link rel="canonical" href="https://www.youtube.com/channel/([^"]+)">', re.I)
+def resolve_user_channel(url, retry=5):
+    try:
+        body = requests.get(url, verify=False).text
+        match = re_link_canonical.search(body)
+        if match:
+            return match.group(1)
+    except Exception as e:
+        if retry:
+            time.sleep(1)
+            return resolve_user_channel(url, retry-1)
+        print >> sys.stderr, "WARNING: could not resolve channel for user %" % uid
 
-def get_channel_from_video_id(vid):
-    pass
+def get_channel_from_user_id(uid, url):
+    if uid in CACHE_USERS_CHANNELS:
+        return CACHE_USERS_CHANNELS[uid]
+    channel = resolve_user_channel(url)
+    if channel:
+        CACHE_USERS_CHANNELS[uid] = channel
+        with open(".cache_users_channels.json", "w") as f:
+            json.dump(CACHE_USERS_CHANNELS, f)
+    return channel
+
+def get_channel_from_video_id(vid, cache_videos_channels={}):
+    if vid in cache_videos_channels:
+        return cache_videos_channels[vid]
+    if channel:
+        CACHE_USERS_CHANNELS[uid] = channel
+        with open(".cache_users_channels.json", "w") as f:
+            json.dump(CACHE_USERS_CHANNELS, f)
+    return channel
+
+def get_cache_videos_channels(csvfile="full_videos.csv", vid_field="yt_video_id", cid_field="yt_channel_id"):
+    cache = {}
+    try:
+        csv.field_size_limit(sys.maxsize)
+        with open(csvfile) as f:
+            for v in csv.DictReader(f):
+                cache[v[vid_field]] = v[cid_field]
+        return cache
+    except Exception as e:
+        print >> sys.stderr, "ERROR %s %s, could not read videos metas csv %s with fields %s %s" % (type(e), e, csvfile, vid_field, cid_field)
+        return cache
 
 if __name__ == "__main__":
     import csv
-    import sys
     csv_file = sys.argv[1] if len(sys.argv) > 1 else "youtube-inlinks.csv"
     csv_field = sys.argv[2] if len(sys.argv) > 2 else "youtube_url"
+    cache_videos_channels = get_cache_videos_channels()
+    print "webentity_id,source_url,target_url,yt_channel_id"
     with open(csv_file) as f:
         for l in csv.DictReader(f):
             try:
-                url_type, url_id = parse_youtube_url(l[csv_field])
-                print(l[csv_field], url_type, url_id)
+                channel = get_channel_from_url(l[csv_field])
+                if channel:
+                    print ",".join([l["webentity"], l["source_url"], l[csv_field], channel])
             except Exception as e:
-                print("ERROR %s: %s" % (type(e), e), l)
-                exit(1)
+                print >> sys.stderr, "ERROR %s: %s" % (type(e), e), l
 
 
